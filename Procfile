e330a3e4c5d4f3d72202a8d0a3bc0efe5ce7d512
veryflying <veryflying@gmail.com>
2015-10-28 18:44:56 +0800

    'first'

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: python h.py ${PORT}
---
+++ h.py
@@ -0,0 +1,42 @@
+import web
+import os, sys
+import test
+
+urls = (
+    '/hello/(.*)', 'Hello',
+    '/t/(.*)', 'translate'
+)
+
+app = web.application(urls, globals())
+
+
+class Hello:
+    def __init__(self):
+        pass
+
+
+    def GET(self, name):
+        if not name:
+            name = 'World'
+        web.setcookie('cookie', 2, 100, secure=True)
+        cookies = web.cookies()
+        print cookies
+        return 'Hello, 1' + name + '!'
+
+
+class Translate:
+    def __init__(self):
+        pass
+
+
+    def GET(self, name):
+        if not name:
+            name = 'a'
+        result = test.get_interp(name)
+        return result
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 80))
+    # sys.argv[1] = port
+    app.run()
